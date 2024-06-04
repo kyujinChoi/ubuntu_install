8e0c8cf1914ea7127b8ed106a8243a03c6ece677
qjinchoi <qjinchoi@gmail.com>
2024-06-04 14:44:25 +0900

    added ros humble

--- 240604_ubuntu22_install.sh
+++ 240604_ubuntu22_install.sh
@@ -129,7 +129,7 @@ echo "** Install libLAS-1.8.1 successfully"
 echo "** ros2 humble install"
 sudo apt update && sudo apt install -y curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install -y ros-humble-desktop
 sudo apt install -y ros-humble-ros-base
